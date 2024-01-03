7ef83b2e2bce064ef851615fe08eca62dca84c01
BlooooopyBleep <154707517+BlooooopyBleep@users.noreply.github.com>
2024-01-02 23:04:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,108 +1,138 @@
-var xhr = new XMLHttpRequest;
-xhr.withCredentials = !0;
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
 var selectedTextBox = null;
-
+xhr.addEventListener("readystatechange", function() {
+  if(this.readyState === 4) {
+    console.log(this.responseText);
+    var split = this.responseText.slice(1,-1).split(",");
+    loadData(split);
+  }
+});
+xhr.open("GET", "https://play.william-duan.games:8443/api/v1/dailypuzzle");
+xhr.send();
 function test() {
-	var e = new XMLHttpRequest;
-	e.withCredentials = !0, e.addEventListener("readystatechange", function() {
-		4 === this.readyState && (console.log(this.responseText), loadData(this.responseText.slice(1, -1).split(",")))
-	}), e.open("GET", "https://play.william-duan.games:8080/api/v1/dailypuzzle"), e.send()
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+  var selectedTextBox = null;
+  xhr.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+      console.log(this.responseText);
+      var split = this.responseText.slice(1,-1).split(",");
+      loadData(split);
+    }
+  });
+  xhr.open("GET", "https://play.william-duan.games:8080/api/v1/dailypuzzle");
+  xhr.send();
 }
+function loadData(data) {
+  for (var wordNum = 0; wordNum < data.length; wordNum++) {
+    document.getElementById("box" + (wordNum + 1)).innerHTML = data[wordNum];
+    console.log(data[wordNum] + " loaded");
+  }
 
-function loadData(e) {
-	for (var t = 0; t < e.length; t++) document.getElementById("box" + (t + 1)).innerHTML = e[t], console.log(e[t] + " loaded")
 }
-
 function resetGrid() {
   for (var wordNum = 1; wordNum <= 16; wordNum++) {
-    document.getElementById("box" + wordNum).innerHTML = e[wordNum - 1];
+    document.getElementById("box" + wordNum).innerHTML = data[wordNum - 1];
   }
   for (var wordNum = 1; wordNum <= 16; wordNum++) {
     document.getElementById("num" + wordNum).innerHTML = "&nbsp;";
   }
 }
-
-function toggleRectangle(e) {
-	var t = document.getElementById(e);
-	t.style.display = "none" === t.style.display ? "block" : "none"
+document.querySelector('.buttonLeft').addEventListener('click', resetGrid);
+document.querySelector('.buttonLeft').addEventListener('touchend', resetGrid);
+function toggleRectangle(rectangleId) {
+  var rectangle = document.getElementById(rectangleId);
+  rectangle.style.display = rectangle.style.display === 'none' ? 'block' : 'none';
 }
-
-function closeRectangle(e) {
-	document.getElementById(e).style.display = "none"
+function closeRectangle(rectangleId) {
+  var rectangle = document.getElementById(rectangleId);
+  rectangle.style.display = 'none';
 }
-
-function handleDragStart(e) {
-	e.dataTransfer.setData("text/plain", e.target.id)
+function handleDragStart(event) {
+  event.dataTransfer.setData("text/plain", event.target.id);
 }
-
-function handleDrop(e) {
-	e.preventDefault();
-	var t = e.dataTransfer.getData("text/plain"),
-		n = document.getElementById(t);
-	if (e.target.classList.contains("grid-item") && !e.target.classList.contains("selected")) {
-		var l = e.target.innerHTML;
-		e.target.innerHTML = n.innerHTML, n.innerHTML = l
-	}
+function handleDrop(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("text/plain");
+  var draggedElement = document.getElementById(data);
+  if (event.target.classList.contains('grid-item') && !event.target.classList.contains('selected')) {
+    var targetText = event.target.innerHTML;
+    event.target.innerHTML = draggedElement.innerHTML;
+    draggedElement.innerHTML = targetText;
+  }
 }
-
-function handleDragOver(e) {
-	e.preventDefault()
+function handleDragOver(event) {
+  event.preventDefault();
 }
-xhr.addEventListener("readystatechange", function() {
-	4 === this.readyState && (console.log(this.responseText), loadData(this.responseText.slice(1, -1).split(",")))
-}), xhr.open("GET", "https://play.william-duan.games:8443/api/v1/dailypuzzle"), xhr.send();
-var gridItems = document.querySelectorAll(".grid-item");
-
-function openMenu(e) {
-	document.getElementById(e).style.display = "block"
+var gridItems = document.querySelectorAll('.grid-item');
+gridItems.forEach(function (item) {
+  item.setAttribute('draggable', true);
+  item.addEventListener('dragstart', handleDragStart);
+});
+document.addEventListener('dragover', handleDragOver);
+document.addEventListener('drop', handleDrop);
+function openMenu(menuId) {
+  var menu = document.getElementById(menuId);
+  menu.style.display = 'block';
 }
-
 function closeMenu() {
-	for (var e = document.getElementsByClassName("menu"), t = 0; t < e.length; t++) e[t].style.display = "none"
+  var menus = document.getElementsByClassName('menu');
+  for (var i = 0; i < menus.length; i++) {
+    menus[i].style.display = 'none';
+  }
 }
-gridItems.forEach(function(e) {
-	e.setAttribute("draggable", !0), e.addEventListener("dragstart", handleDragStart)
-}), document.addEventListener("dragover", handleDragOver), document.addEventListener("drop", handleDrop);
 var selectedTextBox = null;
-
-function handleTouchStart(e) {
-	e.preventDefault();
-	var t = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY);
-	t && t.classList.contains("grid-item") && (highlightBox(selectedTextBox = t), selectedTextBox.style.transition = "none", selectedTextBox.style.pointerEvents = "none", selectedTextBox.style.pointerEvents = "none")
+function handleTouchStart(event) {
+  event.preventDefault();
+  var touchedElement = document.elementFromPoint(event.touches[0].clientX, event.touches[0].clientY);
+  if (touchedElement && touchedElement.classList.contains('grid-item')) {
+    selectedTextBox = touchedElement;
+    highlightBox(selectedTextBox);
+    selectedTextBox.style.transition = 'none';
+    selectedTextBox.style.pointerEvents = 'none'; // Disable hitbox
+    selectedTextBox.style.pointerEvents = 'none'; 
+  }
 }
-
-function handleTouchMove(e) {
-	if (e.preventDefault(), selectedTextBox) {
-		var t = selectedTextBox.getBoundingClientRect(),
-			n = t.width / 2,
-			l = t.height / 2,
-			s = window.pageXOffset || document.documentElement.scrollLeft,
-			a = window.pageYOffset || document.documentElement.scrollTop;
-		selectedTextBox.style.left = e.touches[0].clientX - n + s + "px", selectedTextBox.style.top = e.touches[0].clientY - l + a + "px"
-	}
+function handleTouchMove(event) {
+  event.preventDefault();
+  if (selectedTextBox) {
+    var rect = selectedTextBox.getBoundingClientRect();
+    var offsetX = rect.width / 2;
+    var offsetY = rect.height / 2;
+    var scrollX = window.pageXOffset || document.documentElement.scrollLeft;
+    var scrollY = window.pageYOffset || document.documentElement.scrollTop;
+    selectedTextBox.style.left = event.touches[0].clientX - offsetX + scrollX + 'px';
+    selectedTextBox.style.top = event.touches[0].clientY - offsetY + scrollY + 'px';
+  }
 }
-
-function handleTouchEnd(e) {
-	if (e.preventDefault(), selectedTextBox) {
-		selectedTextBox.style.transition = "", selectedTextBox.style.left = "", selectedTextBox.style.top = "", selectedTextBox.style.pointerEvents = "auto", document.querySelectorAll(".grid-item").forEach(function(e) {
-			e.classList.remove("highlighted")
-		});
-		var t = document.elementFromPoint(e.changedTouches[0].clientX, e.changedTouches[0].clientY);
-		if (t && t.classList.contains("grid-item") && !t.classList.contains("selected")) {
-			var n = selectedTextBox.innerHTML;
-			selectedTextBox.innerHTML = t.innerHTML, t.innerHTML = n
-		}
-		selectedTextBox = null
-	}
+function handleTouchEnd(event) {
+  event.preventDefault();
+  if (selectedTextBox) {
+    selectedTextBox.style.transition = ''; // Reset transition
+    selectedTextBox.style.left = '';
+    selectedTextBox.style.top = '';
+    selectedTextBox.style.pointerEvents = 'auto'; // Enable hitbox
+    document.querySelectorAll('.grid-item').forEach(function (box) {
+      box.classList.remove('highlighted');
+    });
+    var target = document.elementFromPoint(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
+    if (target && target.classList.contains('grid-item') && !target.classList.contains('selected')) {
+      var data = selectedTextBox.innerHTML;
+      selectedTextBox.innerHTML = target.innerHTML;
+      target.innerHTML = data;
+    }
+    selectedTextBox = null;
+  }
 }
-
-function highlightBox(e) {
-	document.querySelectorAll(".grid-item").forEach(function(e) {
-		e.classList.remove("highlighted")
-	}), e.classList.contains("grid-item") && !e.classList.contains("selected") && e.classList.add("highlighted")
+function highlightBox(touchedTextBox) {
+  document.querySelectorAll('.grid-item').forEach(function (box) {
+    box.classList.remove('highlighted');
+  });
+  if (touchedTextBox.classList.contains('grid-item') && !touchedTextBox.classList.contains('selected')) {
+    touchedTextBox.classList.add('highlighted');
+  }
 }
-document.addEventListener("touchstart", handleTouchStart, {
-	passive: !1
-}), document.addEventListener("touchmove", handleTouchMove, {
-	passive: !1
-}), document.addEventListener("touchend", handleTouchEnd);
+document.addEventListener('touchstart', handleTouchStart, { passive: false });
+document.addEventListener('touchmove', handleTouchMove, { passive: false });
+document.addEventListener('touchend', handleTouchEnd);
