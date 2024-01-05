52ed9eff5ecc2ac4593320a2379ddcf94497556b
BlooooopyBleep <154707517+BlooooopyBleep@users.noreply.github.com>
2024-01-05 12:26:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
 
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-var selectedTextBox = null;
-var data;
-xhr.addEventListener("readystatechange", function() {
-  if(this.readyState === 4) {
-    console.log(this.responseText);
-    var split = this.responseText.slice(1,-1).split(",");
-    loadData(split);
-  }
-});
-xhr.open("GET", "https://play.william-duan.games:8443/api/v1/dailypuzzle");
-xhr.send();
-function test() {
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-  var selectedTextBox = null;
-  xhr.addEventListener("readystatechange", function() {
-    if(this.readyState === 4) {
-      console.log(this.responseText);
-      var split = this.responseText.slice(1,-1).split(",");
-      loadData(data);
-    }
-  });
-  xhr.open("GET", "https://play.william-duan.games:8443/api/v1/dailypuzzle");
-  xhr.send();
-}
+var data = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l","m","n","o","p"];
+
 
 
 function loadData(data) {
@@ -148,7 +123,7 @@ function handleTouchEnd(event) {
     document.querySelectorAll('.grid-item').forEach(function (box) {
       box.classList.remove('highlighted');
     });
-    
+
     // Check if the target has the redx class
     if (event.target.classList.contains('redx')) {
       // Handle logic for dropping onto elements with redx class
@@ -163,7 +138,7 @@ function handleTouchEnd(event) {
         target.innerHTML = data;
       }
     }
-    
+
     selectedTextBox = null;
   }
 }
